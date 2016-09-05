10223d737dbcee4da8cf16ea5c83c1ba3e62f8a2
Tae-hoon Kang <hmam3@naver.com>
2016-09-05 18:09:09 +0900

    강태훈

---
+++ Taehoon/CommonTypes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum State
+{
+    Idle,
+    Move,
+    Attack,
+    Hit,
+    Dead,
+}
+
+public struct HitInfo
+{
+    public string name;
+    public int damage;
+}
