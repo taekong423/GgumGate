6b663fd75842953058084dafd89e32b5854f3302
Tae-hoon Kang <hmam3@naver.com>
2016-09-11 16:00:32 +0900

    강태훈

--- GGum_prototype/Assets/Script/Character/PlayerCharacter.cs
+++ GGum_prototype/Assets/Script/Character/PlayerCharacter.cs
@@ -1,42 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerCharacter : Character {
+public class PlayerCharacter : Character
+{
 
+    public int maxJumps;
+
+    private int jumpCounter;
     private float invincibleTime;
     private bool canShoot = true;
     private float hAxis;
     private float vAxis;
     private LayerMask layerMask;
-    private float distToGround;
+    private float rayLength;
+    private Vector3 rayPosCenter;
+    private Vector3 rayPosRight;
+    private Vector3 rayPosLeft;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(InitState());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        onGround = GroundCheck();
-        KeyInput();
-	}
+    float rayPosX;
+    float rayPosY;
 
-    void FixedUpdate()
+    // Use this for initialization
+    void Start()
     {
-        
+        CurrentHP = MaxHP;
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_collider = GetComponent<BoxCollider2D>();
+        layerMask = LayerMask.GetMask("Ground");
+        rayLength = 1.0f;
+        rayPosX = 2.1f;
+        rayPosY = 11.5f;
     }
 
-    private void KeyInput()
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = GroundCheck();
+        KeyInput();
+    }
+
+    void FixedUpdate()
     {
         hAxis = GameController.This.ButtonAxis(EButtonCode.MoveX);
         vAxis = GameController.This.ButtonAxis(EButtonCode.MoveY);
 
-        //Move(Axis.Horizontal, hAxis);
+        Flip(hAxis);
+        Move(Axis.Horizontal, hAxis);
+    }
 
-        if (GameController.This.ButtonDown(EButtonCode.Jump) && onGround)
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Gizmos.DrawLine(rayPosCenter, rayPosCenter - new Vector3(0, y + rayLength, 0));
+        Gizmos.DrawLine(rayPosRight, rayPosRight - new Vector3(0, rayPosY + rayLength, 0));
+        Gizmos.DrawLine(rayPosLeft, rayPosLeft - new Vector3(0, rayPosY + rayLength, 0));
+    }
+
+    private void KeyInput()
+    {
+        if (GameController.This.ButtonDown(EButtonCode.Jump))
         {
-            onGround = false;
-            Jump();
+            if (onGround)
+            {
+                jumpCounter = 0;
+                m_rigidbody.gravityScale = 20;
+            }
+
+            if (jumpCounter < maxJumps)
+            {
+                Jump();
+                m_rigidbody.gravityScale = 50;
+                jumpCounter++;
+            }
         }
 
         if (GameController.This.ButtonPress(EButtonCode.Attack) && canShoot)
@@ -53,7 +88,24 @@ public class PlayerCharacter : Character {
 
     private bool GroundCheck()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distToGround + 0.1f, layerMask);
+        rayPosCenter = transform.position;
+        rayPosRight = rayPosCenter + new Vector3(rayPosX, 0, 0);
+        rayPosLeft = rayPosCenter + new Vector3(-rayPosX, 0, 0);
+        if (IsGround(rayPosLeft) || IsGround(rayPosRight))
+        {
+            m_rigidbody.gravityScale = 10;
+            return true;
+        }
+        else
+        {
+            m_rigidbody.gravityScale = 50;
+            return false;
+        }
+    }
+
+    private bool IsGround(Vector3 originPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(originPos, Vector2.down, rayPosY + rayLength, layerMask);
         return (hit.collider != null);
     }
 
@@ -73,65 +125,9 @@ public class PlayerCharacter : Character {
     IEnumerator Shoot()
     {
         canShoot = false;
-        if (onGround)
-            
         Attack(new HitInfo(gameObject, AttackDamage));
+
         yield return new WaitForSeconds(GetAttackSpeed(AttackSpeed));
         canShoot = true;
-    }
-
-    IEnumerator InitState()
-    {
-        distToGround = m_collider.bounds.extents.y;
-        layerMask = LayerMask.NameToLayer("Ground");
-        m_rigidbody = GetComponent<Rigidbody2D>();
-        yield return null;
-
-        state = State.Idle;
-        yield return null;
-
-        NextState();
-    }
-
-    IEnumerator IdleState()
-    {
-        
-        yield return null;
-
-        while (state == State.Idle)
-        {
-            GetArrowKey();
-            if (hAxis != 0)
-            {
-                state = State.Move;
-            }
-
-            yield return null;
-        }
-
-        NextState();
-    }
-
-    IEnumerator MoveState()
-    {
-        Flip(hAxis);
-        //mesh.animation.CrossFade("death");
-        yield return null;
-
-        while (state == State.Move)
-        {
-            GetArrowKey();
-            if (hAxis == 0)
-            {
-                state = State.Idle;
-            }
-            else
-            {
-                Move(Axis.Horizontal, hAxis);
-            }
-            yield return null;
-        }
-
-        NextState();
     }
 }
