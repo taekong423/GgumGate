fbf83b05d4cb8b6a3213327eb2bd147f45101151
Tae-hoon Kang <hmam3@naver.com>
2016-09-19 18:30:06 +0900

    강태훈
    Scene 수정
    Rafflesia 추가

---
+++ GGum_prototype/Assets/Script/Object/Rafflesia.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rafflesia : MonoBehaviour {
+
+    public float pushForce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy")
+        {
+            Rigidbody2D m_rigidbody = other.gameObject.GetComponent<Rigidbody2D>();
+            m_rigidbody.velocity = Vector2.zero;
+            m_rigidbody.AddForce(new Vector2(-1, 1) * pushForce, ForceMode2D.Impulse);
+        }
+    }
+}
