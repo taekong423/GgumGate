d2f6cdc2cf931abac869c2808e6a5d3706575f29
dydgus5120 <dydgus5120@gmail.com>
2016-10-03 17:48:51 +0900

    칼루트
    사운드 매니저 임시 구현

--- GGum_prototype/Assets/Script/GameSystem/SoundManager.cs
+++ GGum_prototype/Assets/Script/GameSystem/SoundManager.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections;
 
 public class SoundManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public AudioMixer mixer;
+
+    
+    public void SetMasterVolume(float volume)
+    {
+        mixer.SetFloat("Master", volume);
+    }
+    
+    public void SetBGMVolume(float volume)
+    {
+        mixer.SetFloat("BGM", volume);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        mixer.SetFloat("Effect", volume);
+    }
+
+    public void ToggleMasterVolume()
+    {
+        float volume;
+
+        mixer.GetFloat("Master", out volume);
+
+        volume = volume == 0 ? -80 : 0;
+
+        SetMasterVolume(volume);
+    }
+
+    public void ToggleBGMVolume()
+    {
+        float volume;
+
+        mixer.GetFloat("BGM", out volume);
+
+        volume = volume == 0 ? -80 : 0;
+
+        SetBGMVolume(volume);
+    }
+
+    public void ToggleEffectVolume()
+    {
+        float volume;
+
+        mixer.GetFloat("Effect", out volume);
+
+        volume = volume == 0 ? -80 : 0;
+
+        SetEffectVolume(volume);
+    }
+
 }
