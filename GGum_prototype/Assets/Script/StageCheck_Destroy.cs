9dcac79a5c061d3efd792d514aab26a812108e59
dydgus5120 <dydgus5120@gmail.com>
2016-10-20 21:29:35 +0900

    칼루트
    도토리 안 사라지는거 수정

---
+++ GGum_prototype/Assets/Script/StageCheck_Destroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageCheck_Destroy : MonoBehaviour {
+
+    GameManager _gm;
+
+    public string _stageName;
+
+    void Awake()
+    {
+        _gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (!_gm.stages[_gm.currentStageNumber].name.Equals(_stageName))
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
