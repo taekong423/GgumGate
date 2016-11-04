8f83b16c1aee820cc3059efe072eeb4390996814
Tae-hoon Kang <hmam3@naver.com>
2016-11-04 17:06:48 +0900

    강태훈
    말풍선 수정중

---
+++ GGum_prototype/Assets/Script/UI/SpeechBubble.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeechBubble : MonoBehaviour {
+
+    public enum SpeechType
+    {
+        Pet,
+        Player,
+    }
+
+    [System.Serializable]
+    public struct Speech
+    {
+        public SpeechType type;
+        public string text;
+    }
+
+    public int currentSpeech;
+    public Speech[] speechList;
+    
+    private Text petText;
+    private Text playerText;
+
+
+    // Use this for initialization
+    void Start () {
+        petText = GameObject.Find("PetText").GetComponent<Text>();
+        playerText = GameObject.Find("PlayerText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
