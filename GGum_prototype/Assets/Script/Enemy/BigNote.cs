8d8457ce74aaf1c6227cdd68284c532709f91447
dydgus5120 <dydgus5120@gmail.com>
2016-11-15 07:27:30 +0900

    칼루트

--- GGum_prototype/Assets/Script/Enemy/BigNote.cs
+++ GGum_prototype/Assets/Script/Enemy/BigNote.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class BigNote : NewEnemy {
 
+    IEnemy _owner;
+
     public Image _hpBar;
 
+    public float _fallSpeed;
+    
     protected override void Init()
     {
         base.Init();
@@ -18,6 +22,30 @@ public class BigNote : NewEnemy {
     protected override void HitEvent()
     {
         _hpBar.fillAmount = (float)CurrentHP / (float)MaxHP;
+    }
+
+    public void Setting(IEnemy owner, float fallSpeed = 0)
+    {
+        _owner = owner;
+        if (fallSpeed != 0)
+            _fallSpeed = fallSpeed;
+    }
+
+
+    class CreateState : State
+    {
+        readonly BigNote _note;
+
+        public CreateState(BigNote note, string id) : base(id)
+        {
+            _note = note;
+        }
+
+        public override void Enter()
+        {
+            _note._rigidbody.gravityScale = 0;
+        }
+
     }
 
 }
